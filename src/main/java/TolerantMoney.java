59116902b2bc8efaa31300b5de3cc4f132c9e183
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2021-08-26 14:55:20 +0530

    Update TolerantMoney.java
    i use comment in this code 
    thank you please accept my request

--- src/main/java/TolerantMoney.java
+++ src/main/java/TolerantMoney.java
@@ -1,11 +1,11 @@
-public class TolerantMoney {
+public class TolerantMoney { //create a public class tolerantmoney
     private double amount;
 
-    public TolerantMoney(double amount) {
+    public TolerantMoney(double amount) {  //create a constructor 
         this.amount = amount;
     }
 
-    public boolean equals(Object other) {
+    public boolean equals(Object other) { // create a boolean function which take instance of object other 
         if (other instanceof TolerantMoney) {
             TolerantMoney that = (TolerantMoney) other;
             return Math.abs(this.amount - that.amount) <= 0.005d;
